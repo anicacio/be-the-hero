0a1981b66978ea151adcc37c2d8054ade383e438
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-03-29 18:48:42 -0300

    Install React-Icons Logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './styles.css';
+import heroesImage from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
 
 export default function Logon({children}) {
     return (
-        <header>
-            <h1>Hello World!</h1>
-        </header>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+                    <a href="/register">Não tenho cadastro</a>
+                </form>
+
+            </section>
+
+            <img src={heroesImage} alt="Heroes"/>
+        </div>
     );
 };

1d8ed1dc9d2e052f01fd4c71213cc34b3092341c
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-03-29 19:58:06 -0300

    Register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiLogIn} from 'react-icons/fi';
 import './styles.css';
 import heroesImage from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
@@ -11,12 +13,13 @@ export default function Logon({children}) {
                 <form>
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
-                    <button type="submit">Entrar</button>
-                    <a href="/register">Não tenho cadastro</a>
+                    <button className="button"type="submit">Entrar</button>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color='#E02041'/>
+                        Não tenho cadastro
+                    </Link>
                 </form>
-
             </section>
-
             <img src={heroesImage} alt="Heroes"/>
         </div>
     );
